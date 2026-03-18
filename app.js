a8ce11f77f51b1efc061e89ee7cc580c2e461049
Louie Camacho <louiecamacho@gmail.com>
2026-03-18 08:21:57 -0500

    seed dummy films, add starter film query and api files

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 export default app;
 
 import usersRouter from "#api/users";
+import filmsRouter from "#api/films";
 import getUserFromToken from "#middleware/getUserFromToken";
 import handlePostgresErrors from "#middleware/handlePostgresErrors";
 import cors from "cors";
@@ -20,6 +21,7 @@ app.use(getUserFromToken);
 app.get("/", (req, res) => res.send("Hello, World!"));
 
 app.use("/users", usersRouter);
+app.use("/films", filmsRouter);
 
 app.use(handlePostgresErrors);
 app.use((err, req, res, next) => {
