4a194b0be76170db0f56c7d429c88a58d4c78557
Ariana V <arlexavega@gmail.com>
2026-03-30 07:54:25 -0400

    updated group_watches.js with two new queries for updating and deleting and a minor edit to the getCurrentWatchByGroupId query

--- db/queries/group_watches.js
+++ db/queries/group_watches.js
@@ -89,13 +89,55 @@ export async function getWatchesByGroupId(group_id) {
 
 export async function getCurrentWatchByGroupId(group_id) {
   const sql = `
-    SELECT group_watches.*, films.title, films.poster_url, films.year, films.director, films.runtime, films.genre, films.description
+    SELECT 
+    group_watches.*, 
+    films.title, films.poster_url, films.year, films.director, films.runtime, films.genre, films.description,
+    watch_groups.creator_id AS group_creator_id, watch_groups.name AS group_name
     FROM group_watches
     JOIN films ON group_watches.film_id = films.id
+    JOIN watch_groups ON group_watches.group_id = watch_groups.id
     WHERE group_watches.group_id = $1 AND group_watches.status = 'watching'
     ORDER BY group_watches.deadline ASC
     LIMIT 1
   `;
   const { rows: [watch] } = await db.query(sql, [group_id]);
   return watch;
+}
+
+/**
+ * Host updates a watch event's film, deadline, and discussion prompt.
+ *
+ * @param {number} id - ID of the group_watches row to update.
+ * @param {string} film_id - New film ID.
+ * @param {Date} deadline - New deadline.
+ * @param {string} discussion_prompt - New discussion prompt (may be empty).
+ */
+
+export async function updateWatchEvent(id, film_id, deadline, discussion_prompt) {
+  const sql = `
+  UPDATE group_watches
+  SET film_id = $1, deadline = $2, discussion_prompt = $3
+  WHERE id = $4
+  RETURNING *
+  `;
+
+  const { rows : [updated] } = await db.query(sql, [film_id, deadline, discussion_prompt, id]);
+  return updated;
+}
+
+/**
+ * Host deletes a specific watch event by ID.
+ *
+ * @param {number} id - ID of the group_watches row to delete.
+ */
+
+export async function deleteWatchEvent (id) {
+  const sql = ` 
+  DELETE from group_watches
+  WHERE id = $1
+  RETURNING *
+  `;
+
+  const { rows: [deleted] } = await db.query(sql, [id]);
+  return deleted;
 }
